cabf4f4df798011d87f8e1d90858fdc8ea34103a
Khoirul Malik <khoirulmalik03@gmail.com>
2026-01-15 19:34:00 +0700

    update

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,6 +1,6 @@
 // lib/api.ts
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://47.129.44.107:3000";
+  process.env.NEXT_PUBLIC_API_URL || "https://api-ai.desa-maileppet.com:3000";
 
 export const api = {
   // PDF Management
